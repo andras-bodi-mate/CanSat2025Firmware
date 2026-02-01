34aef0a8d40a1a69d4a4f65353d0f4cf02d737d7
Vince12341 <vascsakvince@gmail.com>
2026-02-01 13:32:59 +0100

    implemented fuction

--- src/climateSensor.cpp
+++ src/climateSensor.cpp
@@ -1,10 +1,28 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_BME280.h>
+
 #include "climateSensor.hpp"
 
-ClimateSensor::ClimateSensor(Pin sclPin, Pin sdaPin): sclPin(sclPin), sdaPin(sdaPin) {}
+static Adafruit_BME280 bme;
+
+ClimateSensor::ClimateSensor(Pin sclPin, Pin sdaPin)
+    : sclPin(sclPin), sdaPin(sdaPin) {}
 
 void ClimateSensor::begin() {
+    Wire.begin(sdaPin, sclPin);
+    bme.begin(0x76);
 }
 
 ClimateData ClimateSensor::read() {
-    return ClimateData();
+    if (!bme.sensorID()) {
+        return ClimateData(false);
+    }
+
+    float temperature = bme.readTemperature();
+    float pressure = bme.readPressure() / 1000.0f;
+    float humidity = bme.readHumidity();
+    float altitude = bme.readAltitude(1013.25);
+
+    return ClimateData(true, temperature, pressure, humidity, altitude);
 }
